cd183f068cc08f3e842759aa7771ecf3c40c8465
Marlom Jobsom <marlomjobsom@gmail.com>
2018-06-13 22:10:55 -0300

    Add more APIs for player status

--- core/observations/player_status.py
+++ core/observations/player_status.py
@@ -8,7 +8,15 @@ Read the player status as minerals, vespene  gas, etc ...
 OBSERVATION_PLAYER = 'player'
 
 
-def get_minerals_status(obs):
+def get_player_id(obs):
+    """
+    :param pysc2.env.environment.TimeStep obs:
+    :return numpy.int32:
+    """
+    return obs.observation.get(OBSERVATION_PLAYER)[0]
+
+
+def get_minerals_count(obs):
     """
     :param pysc2.env.environment.TimeStep obs:
     :return numpy.int32:
@@ -16,7 +24,7 @@ def get_minerals_status(obs):
     return obs.observation.get(OBSERVATION_PLAYER)[1]
 
 
-def get_vespene_status(obs):
+def get_vespene_count(obs):
     """
     :param pysc2.env.environment.TimeStep obs:
     :return numpy.int32:
@@ -24,7 +32,7 @@ def get_vespene_status(obs):
     return obs.observation.get(OBSERVATION_PLAYER)[2]
 
 
-def get_current_supply_status(obs):
+def get_used_supply_count(obs):
     """
     :param pysc2.env.environment.TimeStep obs:
     :return numpy.int32:
@@ -32,9 +40,33 @@ def get_current_supply_status(obs):
     return obs.observation.get(OBSERVATION_PLAYER)[3]
 
 
-def get_max_supply_status(obs):
+def get_max_supply_count(obs):
     """
     :param pysc2.env.environment.TimeStep obs:
     :return numpy.int32:
     """
     return obs.observation.get(OBSERVATION_PLAYER)[4]
+
+
+def get_supply_army_count(obs):
+    """
+    :param pysc2.env.environment.TimeStep obs:
+    :return numpy.int32:
+    """
+    return obs.observation.get(OBSERVATION_PLAYER)[5]
+
+
+def get_supply_workers_count(obs):
+    """
+    :param pysc2.env.environment.TimeStep obs:
+    :return numpy.int32:
+    """
+    return obs.observation.get(OBSERVATION_PLAYER)[6]
+
+
+def get_idle_workers_count(obs):
+    """
+    :param pysc2.env.environment.TimeStep obs:
+    :return numpy.int32:
+    """
+    return obs.observation.get(OBSERVATION_PLAYER)[7]
