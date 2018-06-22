6534254f96eb81c21c42368bfd7048b215534402
Daniel Vilas-Boas <daanielvb@gmail.com>
2018-06-22 16:24:19 -0300

    scout returning to base after finding enemy

--- core/bot/terran/scout/ScoutBot.py
+++ core/bot/terran/scout/ScoutBot.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sc2
-from sc2.constants import *
-from sc2 import Race, Difficulty
-from sc2.constants import *
-from sc2.player import Bot, Computer
-from sc2.helpers import ControlGroup
-
-# When using heritage, debug mode isn't working
-from core.bot.terran.generic_terran_bot import GenericTerranBot
-
 
 class Scout(sc2.BotAI):
+
+    cmd_center = None
     current_scout = None
     found_enemy_base = False
     enemy_start_position = None
@@ -20,8 +13,10 @@ class Scout(sc2.BotAI):
     current_idle_units = None
 
     async def on_step(self, iteration):
-        Scout.current_idle_units = self.units.idle.not_structure
 
+        Scout.current_idle_units = self.units.idle.not_structure
+        if Scout.cmd_center is None:
+            Scout.cmd_center = self.units.structure[0]
         # Select worker to be the scouter
         if Scout.current_scout is None:
             Scout.current_scout = self.workers[0]
@@ -30,12 +25,12 @@ class Scout(sc2.BotAI):
             if self.known_enemy_structures and not Scout.found_enemy_base:
                 Scout.enemy_start_position = self.known_enemy_structures[0].position
                 Scout.found_enemy_base = True
+
+            # equals isn't working...
             if Scout.current_idle_units and Scout.current_scout.tag == Scout.current_idle_units[0].tag:
                 # Found enemy location, go back to base
                 if Scout.found_enemy_base:
-                    print('Should move to base now')
-                    # The line below is crashing the game, must find a way to send the scouter back to base
-                    # await self.do(Scout.current_scout.move(self.start_location))
+                        await self.do(Scout.current_scout.move(self.start_location))
                 else:
                     # Haven't found enemy yet, go to next location
                     Scout.enemy_location_counter += 1
